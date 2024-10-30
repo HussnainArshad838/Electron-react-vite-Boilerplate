39fcd8156bfb03170d9807cc4c5db38092682556
HussnainArshad838 <hussnainrajpoot5415@gmail.com>
2024-10-31 00:59:26 +0500

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  server:{
-port:3000
+  base: './', // Ensures relative paths for assets
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist', // Specifies the output directory for build files
+    rollupOptions: {
+      // Add Electron's entry file if you have one (for Electron-based projects)
+      input: {
+        main: './electron/main.js' // Change to the main file if using Electron
+      },
+      output: {
+      
+      },
+    },
   },
   plugins: [react()],
 })
